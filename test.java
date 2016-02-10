1c0793e58bc28f5e8c69519662a0297db50407e7
lucasvberkel@gmail.com <lucasvberkel@gmail.com>
2016-02-10 15:42:49 +0100

    Eerste aanpassingen test.java

--- test.java
+++ test.java
@@ -4,41 +4,34 @@ import java.util.*;
 public class test{
 
 	public static void main(String[] args){
-		File file = new File("wordlist.txt");
+		String[] wordList = makeArray("wordlist.txt");
+	}
+
+	public String[] makeArray(String textFile){
+		File file = new File(textFile);
 		BufferedReader firstReader = null;
 		BufferedReader secondReader = null;
 		Integer counter = 0;
 		try {
 		    firstReader = new BufferedReader(new FileReader(file));
 		    String text = null;
-
 		    while ((text = firstReader.readLine()) != null) counter ++;
-		    System.out.println(counter);
 		    secondReader = new BufferedReader(new FileReader(file));
-		    String[] firstDatastructure;
-		    firstDatastructure = new String[counter];
-		    counter = 0;
-		    while ((text = secondReader.readLine()) != null){ 
-		    	firstDatastructure[counter] = text;
-		    	counter ++;
+		    String[] datastructure = new String[counter];
+		    for(int i = 0; i < counter; i++){
+		    	datastructure[i] = secondReader.readLine();
 		    }
-		    for(int i = 638275; i < 638285; i++){
-		    	System.out.println(firstDatastructure[i]);
+		  	if (firstReader != null) {
+		    	firstReader.close();
+		    }
+		    if (secondReader != null) {
+		       	secondReader.close();
 		    }
 		} catch (FileNotFoundException e) {
 		    e.printStackTrace();
 		} catch (IOException e) {
 		    e.printStackTrace();
-		} finally {
-		    try {
-		        if (firstReader != null) {
-		            firstReader.close();
-		            		        }
-		        if (secondReader != null) {
-		        	secondReader.close();
-		        }
-		    } catch (IOException e) {
-		    }
-		}	
+		}
+		return String[] datastructure;	
 	}
 }
