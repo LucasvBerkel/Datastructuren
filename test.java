5acc780564e232ff84d7e07f6491a257feda0926
lucasvberkel@gmail.com <lucasvberkel@gmail.com>
2016-02-10 15:54:56 +0100

    Verdeeld in functie

--- test.java
+++ test.java
@@ -3,26 +3,38 @@ import java.util.*;
 
 public class test{
 
-	public static void main(String[] args){
-		String[] wordList = makeArray("wordlist.txt");
-	}
-
-	public String[] makeArray(String textFile){
+	public static int getLineCount(String textFile){
 		File file = new File(textFile);
 		BufferedReader firstReader = null;
-		BufferedReader secondReader = null;
-		Integer counter = 0;
+		Integer counter = 0;	
 		try {
 		    firstReader = new BufferedReader(new FileReader(file));
 		    String text = null;
 		    while ((text = firstReader.readLine()) != null) counter ++;
+		} catch (FileNotFoundException e) {
+		    e.printStackTrace();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		} finally {
+			try{
+		   		if (firstReader != null) {
+		    		firstReader.close();
+		    	}
+			} catch(IOException e){
+				e.printStackTrace();
+			}
+		}
+		return counter;
+	}
+
+	public static String[] makeArray(String textFile, int counter){
+		File file = new File(textFile);
+		BufferedReader secondReader = null;
+		String[] datastructure = new String[counter];
+		try {
 		    secondReader = new BufferedReader(new FileReader(file));
-		    String[] datastructure = new String[counter];
 		    for(int i = 0; i < counter; i++){
 		    	datastructure[i] = secondReader.readLine();
-		    }
-		  	if (firstReader != null) {
-		    	firstReader.close();
 		    }
 		    if (secondReader != null) {
 		       	secondReader.close();
@@ -32,6 +44,12 @@ public class test{
 		} catch (IOException e) {
 		    e.printStackTrace();
 		}
-		return String[] datastructure;	
+		return datastructure;	
+	}
+
+	public static void main(String[] args){
+		int counter = getLineCount("wordlist.txt");
+		String[] wordList = makeArray("wordlist.txt", counter);
+		System.out.println(wordList[10000]);
 	}
 }
