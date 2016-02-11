6d0c13075672d0e9c8b0de8c69643cb7880167e8
Riny Joel Meyer <RJM@MBP-van-Riny.fritz.box>
2016-02-11 21:12:15 +0100

    volgorde aangepast, simpele comments toegevoegd

--- test.java
+++ test.java
@@ -3,6 +3,12 @@ import java.util.*;
 
 public class test{
 
+	// Run the program.
+	public static void main(String[] args){
+		fileResults();
+	}
+
+	// Count number of lines of file to specify size of array.
 	public static int getLineCount(File file){
 		BufferedReader reader = null;
 		Integer counter = 0;	
@@ -26,6 +32,7 @@ public class test{
 		return counter;
 	}
 
+	// Read file into array datastructure.
 	public static String[] makeArray(File file, int counter){
 		BufferedReader reader = null;
 		String[] datastructure = new String[counter];
@@ -45,10 +52,7 @@ public class test{
 		return datastructure;	
 	}
 
-	public static void main(String[] args){
-		fileResults();
-	}
-
+	// Function to check whether a sample matches one of the words in the wordlist.txt file.
 	public static String checkSample(String[] samples, String[] wordList){
 		int counter = 0;
 		for (int i = 0; i < samples.length; i++){
@@ -62,13 +66,13 @@ public class test{
 		return counter + "/" + samples.length;
 	}
 
+	// Save results in .txt file. 
 	public static void fileResults(){
 		File file = new File("wordlist.txt");
 		int counter = getLineCount(file);
 		String[] wordList = makeArray(file, counter);
 		File dir = new File("Samples/");
 		File listDir[] = dir.listFiles();
-
 		try {
 	      	PrintStream out = new PrintStream(new FileOutputStream("arrayResults.txt"));
 		    for(File f : listDir){
