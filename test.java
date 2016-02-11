eec27609a919fab2bbba20bab8d03d86a0601265
Riny Joel Meyer <RJM@MBP-van-Riny.fritz.box>
2016-02-11 13:22:14 +0100

    zo goed als af, eventueel naam veranderen

--- test.java
+++ test.java
@@ -46,37 +46,44 @@ public class test{
 	}
 
 	public static void main(String[] args){
+		fileResults();
+	}
+
+	public static String checkSample(String[] samples, String[] wordList){
+		int counter = 0;
+		for (int i = 0; i < samples.length; i++){
+			for(int j = 0; j < wordList.length; j ++){
+				if(samples[i].equals(wordList[j])){
+					counter++;
+					break;
+				}
+			}
+		}
+		return counter + "/" + samples.length;
+	}
+
+	public static void fileResults(){
 		File file = new File("wordlist.txt");
 		int counter = getLineCount(file);
 		String[] wordList = makeArray(file, counter);
 		File dir = new File("Samples/");
 		File listDir[] = dir.listFiles();
-		for(File f : listDir){
-			System.out.println(f.getName());
-			counter = getLineCount(f);
-			String[] sampleList = makeArray(f, counter);
-			long startTime = System.currentTimeMillis();
-			checkSample(sampleList, wordList);
-			long endTime   = System.currentTimeMillis();
-			long totalTime = endTime - startTime;
-			System.out.println(totalTime);
-		}
-	}
 
-	public static int checkSample(String[] samples, String[] wordList){
-		int counter = 0;
-		for (int i = 0; i < samples.length; i++){
-			String word1 = samples[i];
-			for(int j = 0; j < wordList.length; j ++){
-				String word2 = wordList[j];
-				if(word1.equals(word2)){
-					counter++;
-					break;
-				}
+		try {
+	      	PrintStream out = new PrintStream(new FileOutputStream("arrayResults.txt"));
+		    for(File f : listDir){
+				out.println("Filename: " + f.getName());
+				counter = getLineCount(f);
+				String[] sampleList = makeArray(f, counter);
+				long startTime = System.nanoTime();
+				out.println("Correct samples/total: " + checkSample(sampleList, wordList));
+				long endTime   = System.nanoTime();
+				long totalTime = endTime - startTime;
+				out.println("Nanoseconds: " + totalTime + '\n');
 			}
-			System.out.println(i);
-		}
-		System.out.println(counter + "/" + samples.length);
-		return counter;
+		    out.close();
+	    } catch (FileNotFoundException e) {
+	      e.printStackTrace();
+	    }
 	}
 }
