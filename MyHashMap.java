bac690a37ef88241cb8c14fa5b37f44ae08d47e1
Riny Joel Meyer <RJM@MacBook-Pro-van-Riny.local>
2016-02-16 01:53:55 +0100

    eindelijk werkt alles(H

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,96 +1,50 @@
-public class MyHashMap extends MyHashEntry{
-      private final int DEFAULT_TABLE_SIZE = 128;
-      private float threshold = 0.75f;
-      private int maxSize = 96;
-      private int size = 0;
- 
-      MyHashEntry[] table;
- 
-      MyHashMap() {
-            table = new MyHashEntry[DEFAULT_TABLE_SIZE];
-            for (int i = 0; i < DEFAULT_TABLE_SIZE; i++)
-                  table[i] = null;
-      }
- 
-      public void setThreshold(float threshold) {
-            this.threshold = threshold;
-            maxSize = (int) (table.length * threshold);
-      }
- 
-      public int get(int key) {
-            int hash = (key % table.length);
-            int initialHash = -1;
-            while (hash != initialHash
-                        && (table[hash] == DeletedEntry.getUniqueDeletedEntry()
-                        || table[hash] != null
-                        && table[hash].getKey() != key)) {
-                  if (initialHash == -1)
-                        initialHash = hash;
-                  hash = (hash + 1) % table.length;
-            }
-            if (table[hash] == null || hash == initialHash)
-                  return -1;
-            else
-                  return table[hash].getValue();
-      }
- 
-      public void put(int key, int value) {
-            int hash = (key % table.length);
-            int initialHash = -1;
-            int indexOfDeletedEntry = -1;
-            while (hash != initialHash
-                        && (table[hash] == DeletedEntry.getUniqueDeletedEntry()
-                        || table[hash] != null
-                        && table[hash].getKey() != key)) {
-                  if (initialHash == -1)
-                        initialHash = hash;
-                  if (table[hash] == DeletedEntry.getUniqueDeletedEntry())
-                        indexOfDeletedEntry = hash;
-                  hash = (hash + 1) % table.length;
-            }
-            if ((table[hash] == null || hash == initialHash)
-                        && indexOfDeletedEntry != -1) {
-                  table[indexOfDeletedEntry] = new MyHashEntry(key, value);
-                  size++;
-            } else if (initialHash != hash)
-                  if (table[hash] != DeletedEntry.getUniqueDeletedEntry()
-                             && table[hash] != null && table[hash].getKey() == key)
-                        table[hash].setValue(value);
-                  else {
-                        table[hash] = new MyHashEntry(key, value);
-                        size++;
-                  }
-            if (size >= maxSize)
-                  resize();
-      }
- 
-      private void resize() {
-            int tableSize = 2 * table.length;
-            maxSize = (int) (tableSize * threshold);
-            MyHashEntry[] oldTable = table;
-            table = new MyHashEntry[tableSize];
-            size = 0;
-            for (int i = 0; i < oldTable.length; i++)
-                  if (oldTable[i] != null
-                             && oldTable[i] != DeletedEntry.getUniqueDeletedEntry())
-                        put(oldTable[i].getKey(), oldTable[i].getValue());
-      }
- 
-      public void remove(int key) {
-            int hash = (key % table.length);
-            int initialHash = -1;
-            while (hash != initialHash
-                        && (table[hash] == DeletedEntry.getUniqueDeletedEntry()
-                        || table[hash] != null
-                        && table[hash].getKey() != key)) {
-                  if (initialHash == -1)
-                        initialHash = hash;
-                  hash = (hash + 1) % table.length;
-            }
-            if (hash != initialHash && table[hash] != null) {
-                  table[hash] = DeletedEntry.getUniqueDeletedEntry();
-                  size--;
-            }
-      }
-}
+import java.io.*;
+import java.util.*;
 
+public class MyHashMap{
+	final int INVALID = 0xFFFFFFFF;
+	MyCluster[] m_clusters;
+
+	public MyHashMap(){
+		m_clusters = new MyCluster[52];
+
+		for (int i = 0; i < m_clusters.length; i++){
+			m_clusters[i] = new MyCluster();
+		}
+	}
+
+	public boolean Put(String word){
+
+		if (word != null){
+			int hash = GetHash(word);
+			if (hash != INVALID){
+				m_clusters[hash].Put(word);
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public boolean Search(String word){
+		int hash = GetHash(word);
+		if (hash != INVALID){
+			return m_clusters[hash].Search(word);
+		}
+		return false;
+	}
+
+	private int GetHash(String word){
+		if (word == null){
+			return INVALID;
+		}
+
+		int hash = INVALID;
+		if ((word.charAt(0) >= 'a') && (word.charAt(0) <= 'z')){
+			hash = word.charAt(0) - 'a';
+		}
+		else if ((word.charAt(0) >= 'A') && (word.charAt(0) <= 'Z')){
+			hash = word.charAt(0) - 'A' + 26;
+		}
+		return hash;
+	}
+}
--- hashTableOANewHash.java
+++ hashTableOANewHash.java
@@ -2,112 +2,75 @@ import java.io.*;
 import java.util.*;
 
 public class hashTableOANewHash{
+
 	public static void main(String[] args){
-		File file = new File("wordlist.txt");
-		int counter = getLineCount(file);
-		//String[] wordList = makeArray(file, counter);
-		//Int[] keys;
-		//for (int i = 0; wordList.length; i++){
-		//	keys[i] = keyFunction(wordList[i]);
-		//}
-		boolean[] hasBeenUsed;
 
+		MyHashMap m_map = new MyHashMap();
+		Writer("wordlist.txt", m_map);
+
+		int foundCount = 0;
+		foundCount += Searcher("sample_0OXg@T=T55.txt", m_map);
+		System.out.println(foundCount);
+
+		while (!terminalCom(m_map));
+	}
+
+	private static void Writer(String fileName, MyHashMap map){
+		File file = new File(fileName);
+		try {
+		    BufferedReader reader = new BufferedReader(new FileReader(file));
+		    while (true){
+		   		String line = reader.readLine();
+		   		if (line == null){
+		   			break;
+		   		}
+		    	map.Put(line); 	 
+		    }
+		} catch (FileNotFoundException e) {
+		    e.printStackTrace();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+	}
+
+	// ipv int class Results, aantal items, hoeveelheid foute items, hoeveelheid goede items
+	// toString functie toevoegen welke string teruggeeft van de results
+	private static int Searcher(String fileName, MyHashMap map){
+		int count = 0;
+		File file = new File(fileName);
+		try {
+		    BufferedReader reader = new BufferedReader(new FileReader(file));
+		    while (true){
+		   		String line = reader.readLine();
+		   		if (line == null){
+		   			break;
+		   		}
+		    	count = map.Search(line) ? count + 1 : count;	 
+		    }
+		} catch (FileNotFoundException e) {
+		    e.printStackTrace();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+		return count;
+	}
+
+	private static boolean terminalCom(MyHashMap map){
 		BufferedReader lineOfText = new BufferedReader(new InputStreamReader(System.in));
-		while (true) {
-			try {
-    			String textLine = lineOfText.readLine();
-    			if (textLine.equals("exit")){
-    				return;
-    			}
-    		}
-    		catch (IOException e) {
-    		}
+		try {
+			String textLine = lineOfText.readLine();
+			if (textLine.equals("exit")){
+				return true;
+			}
+			if (map.Search(textLine)){
+				System.out.println("Word found!");
+			}
+			else {
+				System.out.println("Word not found..");
+			}
+		}
+		catch (IOException e) {
 		}	
+		return false;
 	}
-	/*
-	private static boolean searchFunction(int[] keys, int key){
-		// zolang een van de keys != key of een van de keys = "DeleteMe", 
-		// blijf proberen tot het lukt, dan return true
-		// als geen succes en een lege plek return false
-	}
-
-	private static int insertFunction(String[] keys, int[] value){
-		// blijf proberen tot er een lege plek wordt gevonden, insert
-		// element op die plek. 
-	}
-
-	private static String deleteFunction(int key){
-		// vervang verwijderde item met "DeleteMe"
-	}
-
-	private static int hashFunction(int[] key, int trialCount){
-		// moet een permutatie worden van 0,1,..,m-1   m = wordList.length 
-	}
-	*/
-	private static int keyFunction(String word){
-        int key = 7; 
-        for(int i = 0; i < word.length(); i++){
-            key = (key * 31) + word.charAt(i) % 100;
-        }
-        return key;
-	 }
-
-	// Read file into array datastructure.
-	public static String[] makeArray(File file, int counter){
-		BufferedReader reader = null;
-		String[] datastructure = new String[counter];
-		try {
-		    reader = new BufferedReader(new FileReader(file));
-		    for(int i = 0; i < counter; i++){
-		    	datastructure[i] = reader.readLine();  	 
-		    }
-		    if (reader != null) {
-		       	reader.close();
-		    }
-		} catch (FileNotFoundException e) {
-		    e.printStackTrace();
-		} catch (IOException e) {
-		    e.printStackTrace();
-		}
-		return datastructure;	
-	}
-
-	// Count number of lines of file to specify size of array.
-	public static int getLineCount(File file){
-		BufferedReader reader = null;
-		Integer counter = 0;	
-		try {
-		    reader = new BufferedReader(new FileReader(file));
-		    String text = null;
-		    while ((text = reader.readLine()) != null) counter ++;
-		} catch (FileNotFoundException e) {
-		    e.printStackTrace();
-		} catch (IOException e) {
-		    e.printStackTrace();
-		} finally {
-			try{
-		   		if (reader != null) {
-		    		reader.close();
-		    	}
-			} catch(IOException e){
-				e.printStackTrace();
-			}
-		}
-		return counter;
-	}
-
-	/*// Function to check whether a sample matches one of the words in the wordlist.txt file.
-	public static String checkSample(String[] samples, String[] wordList){
-		int counter = 0;
-		for (int i = 0; i < samples.length; i++){
-			//int startcounter = hashFunction(samples[i]);
-			for(int j = startcounter; j < wordList.length; j ++){
-				if(samples[i].equals(wordList[j])){
-					counter++;
-					break;
-				}
-			}
-		}
-		return counter + "/" + samples.length;
-	}*/
 }
