a43312b489c03f4ae3a98cfff987f40f7ff25c30
Riny Joel Meyer <RJM@wcw-stud-145-109-28-136.wireless.uva.nl>
2016-02-18 19:06:04 +0100

    commented

--- MyArraylist.java
+++ MyArraylist.java
@@ -1,3 +1,7 @@
+// Authors:
+// Lucas van Berkel, 10747958
+// Joël Meyer, 10003539
+
 import java.io.*;
 import java.util.*;
 
@@ -8,7 +12,11 @@ public class MyArraylist{
 	String[] m_array = null;
 	int length;
 
-
+	// Constructor, the object contains three attributes.
+	// Attribute(s):
+	// m_arrayCnt, counter to keep track of already filled indeces.
+	// m_array, array set to its initial size.
+	// length, to be able to retrieve length of object. 
 	public MyArraylist(){
 		m_arrayCnt = 0;
 		m_array = new String[INITIALARRAYSIZE];
@@ -16,6 +24,11 @@ public class MyArraylist{
 		length = 0;
 	} 
 
+	// Search the object for the given word.
+	// Input(s):
+	// - word, the word that needs to be checked.
+	// Output(s):
+	// - true if word is found, false if not.
 	public boolean search(String word){
 		for(int i = 0; i < this.length; i++){
 			if (m_array[i].equals(word)){
@@ -25,6 +38,9 @@ public class MyArraylist{
 		return false;
 	}
 
+	// Put word in the object.
+	// Input(s): 
+	// - word, the word that needs to be saved.
 	public void put(String word){
 		m_array[m_arrayCnt] = word;
 		m_arrayCnt++;
@@ -34,12 +50,18 @@ public class MyArraylist{
 		}
 	}
 
+	// Get the length of the object.
 	public void length(){
 		int counter = 0;
 		while(m_array[counter] != null) counter++;
 		this.length = counter;
 	}
 
+	// Checks if object contains a certain character. 
+	// Input(s):
+	// - c, character that needs to be checked. 
+	// Output(s):
+	// - true if 
 	public boolean contains(char c){
 		String letter = "";
 		for(int i = 0; i < m_array.length; i++){
@@ -54,6 +76,8 @@ public class MyArraylist{
 		return false;
 	}
 
+	// Resize the array if array is full. Old array is saved in buffer,
+	// new array is twice as big as old array.
 	private void resize(){
 		String[] buffer = new String[m_array.length];
 		copy(m_array, buffer, m_array.length);
@@ -62,12 +86,20 @@ public class MyArraylist{
 		clearData(buffer.length);
 	}
 
+	// Copy an array to another array. 
+	// Input(s):
+	// - src, source array that has to be copied.
+	// - dst, destination array that has to be filled.
+	// - length, length of the source of array.
 	private void copy(String[] src, String[] dst, int length){
 		for(int i = 0; i < length; i++){
 			dst[i] = src[i];
 		}
 	}
 
+	// Initialise values of array to null.
+	// Input(s):
+	// - index, index from where to null inputs.
 	private void clearData(int index){
 		for(int i = 0; i < index; i++){
 			m_array[i + index] = null;
