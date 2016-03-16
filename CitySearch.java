8dc82e4f671674d62ee64c74c02dc7b8c502a1de
Riny Joel Meyer <RJM@MacBook-Pro-van-Riny.fritz.box>
2016-03-16 16:51:46 +0100

    imports

--- CitySearch.java
+++ CitySearch.java
@@ -1,8 +1,8 @@
+import java.io.BufferedReader;
 import java.io.Console;
 import java.io.File;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.lang.Object;
 import java.util.Scanner;
